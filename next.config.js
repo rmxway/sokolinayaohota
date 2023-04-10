a9459bca33976543770512c680b7cd8b859a8f90
Evgeny <rmxway@mail.ru>
2023-04-10 11:22:07 +0300

    Project init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true,
+	},
+	webpack(config) {
+		config.resolve.fallback = {
+			...config.resolve.fallback, // if you miss it, all the other options in fallback, specified by next.js will be dropped. Doesn't make much sense, but how it is
+			fs: false,
+		};
 
-module.exports = nextConfig
+		return config;
+	},
+};
