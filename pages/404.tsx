bfc8b934be546427f7c3b9b24b10fb0ed12bce67
rmxway <rmxway@mail.ru>
2023-06-04 16:17:13 +0300

    Change 404 page

--- pages/404.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next';
-
-import { Container } from '@/components/Layout';
-
-const NotFoundPage: NextPage = () => <Container grid center>404</Container>;
-
-export { NotFoundPage };
-export default NotFoundPage;
